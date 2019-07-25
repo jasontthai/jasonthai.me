8172e1d360618b468f2c044d1b8ec8d3f6a17542
Jason Thai <jasontt@amazon.com>
2019-07-25 10:36:47 -0700

    update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,7 +2,7 @@ git config user.name "$USER_NAME"
 git config user.email "$USER_EMAIL"
 
 git clone $GITHUB_PAGE_REPOSITORY_URL destination
-cd source
+cd destination
 
 git checkout master
 git pull origin master
@@ -11,7 +11,7 @@ find . -maxdepth 1 ! -name '_site' ! -name '.git' ! -name '.gitignore' -exec rm 
 mv ../_site/* .
 
 git add -fA
-git commit --allow-empty -m "$(git log source -1 --pretty=%B)"
+git commit --allow-empty -m "$(git log master -1 --pretty=%B)"
 git push -f origin master
 
 echo "Deployed successfully"
