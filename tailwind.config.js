428291ee4fa252a3450c4572d784baf7f0f4d0d3
Jason Thai <thaixtri@gmail.com>
2022-06-08 15:15:29 -0700

    Update font to ibm plex mono

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     ],
     theme: {
         // ...
+        extend: {
+            fontFamily: {
+                'ibm': ['IBM Plex Mono', 'monospace'],
+            },
+        },
     },
     plugins: [
         require('@tailwindcss/typography'),
